e44259108f46f54953e20cb9ec337e27e1b77deb
Ali Shakiba <ali@shakiba.me>
2017-03-22 18:24:42 -0400

    fix stage full-screen position

--- dist/planck-with-testbed.js
+++ dist/planck-with-testbed.js
@@ -1,5 +1,5 @@
 /*
- * Planck.js v0.1.20
+ * Planck.js v0.1.22
  * 
  * Copyright (c) 2016-2017 Ali Shakiba http://shakiba.me/planck.js
  * Copyright (c) 2006-2013 Erin Catto  http://www.gphysics.com
@@ -23,7 +23,7 @@
 /*
  * Stage.js 
  * 
- * @copyright 2016 Ali Shakiba http://shakiba.me/stage.js
+ * @copyright 2017 Ali Shakiba http://shakiba.me/stage.js
  * @license The MIT License
  */
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.planck=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
@@ -13590,6 +13590,9 @@ function AppLoader(app, configs) {
     DEBUG && console.log('Creating Canvas...');
     canvas = document.createElement('canvas');
     canvas.style.position = 'absolute';
+    canvas.style.top = '0';
+    canvas.style.left = '0';
+
     var body = document.body;
     body.insertBefore(canvas, body.firstChild);
   }
