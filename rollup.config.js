56de74b1343000b13bb30dce880d4735cfc113aa
Ali Shakiba <ali@shakiba.me>
2021-03-06 21:23:44 -0800

    add rollup plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,64 @@
-export default {
+import babel from 'rollup-plugin-babel';
+import { terser } from "rollup-plugin-terser";
+import license from 'rollup-plugin-license';
+import replace from '@rollup/plugin-replace';
+import filesize from 'rollup-plugin-filesize';
+
+import licenseBanner from './license';
+
+
+export default [
+  {
     input: 'lib/index.js',
     output: {
-        name: 'planck',
-        file: 'dist/planck.js',
-        format: 'umd'
-    }
-};
+      name: 'planck',
+      file: 'dist/planck.js',
+      format: 'umd',
+      sourcemap: true
+    },
+    plugins: [
+      replace({
+        preventAssignment: true,
+        values: {
+          'DEBUG': JSON.stringify(false),
+          'ASSERT': JSON.stringify(false),
+        },
+      }),
+      babel({
+        runtimeHelpers: true,
+        exclude: 'node_modules/**',
+      }),
+      license({
+        banner: licenseBanner,
+      }),
+      filesize()
+    ]
+  },
+  {
+    input: 'lib/index.js',
+    output: {
+      name: 'planck',
+      file: 'dist/planck.min.js',
+      format: 'umd',
+      sourcemap: true
+    },
+    plugins: [
+      replace({
+        preventAssignment: true,
+        values: {
+          'DEBUG': JSON.stringify(false),
+          'ASSERT': JSON.stringify(false),
+        },
+      }),
+      babel({
+        runtimeHelpers: true,
+        exclude: 'node_modules/**',
+      }),
+      license({
+        banner: licenseBanner,
+      }),
+      terser(),
+      filesize()
+    ]
+  }
+];
