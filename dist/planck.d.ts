3ede11b420e604ce0ada52c905b19a3de9c63acd
Ali Shakiba <ali@shakiba.me>
2021-04-23 21:03:29 -0700

    clean up body def docs

--- dist/planck.d.ts
+++ dist/planck.d.ts
@@ -1559,57 +1559,62 @@ declare class World {
     publish(name: string, arg1?: any, arg2?: any, arg3?: any): number;
 }
 type BodyType = "static" | "kinematic" | "dynamic";
-/**
- * @prop type Body types are static, kinematic, or dynamic. Note: if a dynamic
- *       body would have zero mass, the mass is set to one.
- *
- * @prop position The world position of the body. Avoid creating bodies at the
- *       origin since this can lead to many overlapping shapes.
- *
- * @prop angle The world angle of the body in radians.
- *
- * @prop linearVelocity The linear velocity of the body's origin in world
- *       co-ordinates.
- *
- * @prop angularVelocity
- *
- * @prop linearDamping Linear damping is use to reduce the linear velocity. The
- *       damping parameter can be larger than 1.0 but the damping effect becomes
- *       sensitive to the time step when the damping parameter is large.
- *
- * @prop angularDamping Angular damping is use to reduce the angular velocity.
- *       The damping parameter can be larger than 1.0 but the damping effect
- *       becomes sensitive to the time step when the damping parameter is large.
- *
- * @prop fixedRotation Should this body be prevented from rotating? Useful for
- *       characters.
- *
- * @prop bullet Is this a fast moving body that should be prevented from
- *       tunneling through other moving bodies? Note that all bodies are
- *       prevented from tunneling through kinematic and static bodies. This
- *       setting is only considered on dynamic bodies. Warning: You should use
- *       this flag sparingly since it increases processing time.
- *
- * @prop active Does this body start out active?
- *
- * @prop awake Is this body initially awake or sleeping?
- *
- * @prop allowSleep Set this flag to false if this body should never fall
- *       asleep. Note that this increases CPU usage.
- */
 interface BodyDef {
+    /**
+     * Body types are static, kinematic, or dynamic. Note: if a dynamic
+     * body would have zero mass, the mass is set to one.
+     */
     type?: BodyType;
+    /**
+     * The world position of the body. Avoid creating bodies at the
+     * origin since this can lead to many overlapping shapes.
+     */
     position?: Vec2;
+    /**
+     * The world angle of the body in radians.
+     */
     angle?: number;
+    /**
+     * The linear velocity of the body's origin in world co-ordinates.
+     */
     linearVelocity?: Vec2;
     angularVelocity?: number;
+    /**
+     * Linear damping is use to reduce the linear velocity. The
+     * damping parameter can be larger than 1.0 but the damping effect becomes
+     * sensitive to the time step when the damping parameter is large.
+     */
     linearDamping?: number;
+    /**
+     * Angular damping is use to reduce the angular velocity.
+     * The damping parameter can be larger than 1.0 but the damping effect
+     * becomes sensitive to the time step when the damping parameter is large.
+     */
     angularDamping?: number;
+    /**
+     * Should this body be prevented from rotating? Useful for characters.
+     */
     fixedRotation?: boolean;
+    /**
+     * Is this a fast moving body that should be prevented from
+     * tunneling through other moving bodies? Note that all bodies are
+     * prevented from tunneling through kinematic and static bodies. This
+     * setting is only considered on dynamic bodies. Warning: You should use
+     * this flag sparingly since it increases processing time.
+     */
     bullet?: boolean;
     gravityScale?: number;
+    /**
+     * Set this flag to false if this body should never fall asleep. Note that this increases CPU usage.
+     */
     allowSleep?: boolean;
+    /**
+     * Is this body initially awake or sleeping?
+     */
     awake?: boolean;
+    /**
+     * Does this body start out active?
+     */
     active?: boolean;
     userData?: any;
 }
