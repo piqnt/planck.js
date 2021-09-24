97c029dff8042ccf809e57f1e9eeb5a836a41cae
Oliver Zell <zOadT@users.noreply.github.com>
2021-09-24 20:53:08 +0200

    split class declarations into interface and object

--- declarationTransformer.js
+++ declarationTransformer.js
@@ -9,11 +9,106 @@ function visitor(factory, node, options) {
       !isClassIncluded(node.name.escapedText, options)) {
     return node;
   }
+  if (isClassDerived(node)) {
+    return splitClassIntoConstAndInterface(factory, node);
+  }
   return createNodeWithFactories(factory, node);
 }
 
 function isClassIncluded(className, options) {
   return options.classes.includes(className);
+}
+
+function isClassDerived(node) {
+  return node.heritageClauses[0] !== undefined;
+}
+
+function splitClassIntoConstAndInterface(factory, node) {
+  return [
+    createConstNodeWithStaticMethods(factory, node),
+    createInterfaceWithMethods(factory, node)
+  ];
+}
+
+function createConstNodeWithStaticMethods(factory, node) {
+  return factory.createVariableStatement(
+    [factory.createModifier(ts.SyntaxKind.DeclareKeyword)],
+    factory.createVariableDeclarationList(
+      [factory.createVariableDeclaration(
+        node.name,
+        undefined,
+        factory.createTypeLiteralNode([
+          // constructors
+          ...node.members
+            .filter(member => member.kind === ts.SyntaxKind.Constructor)
+            .flatMap(member => [
+              factory.createConstructSignature(
+                undefined,
+                member.parameters,
+                factory.createTypeReferenceNode(
+                  factory.createIdentifier(node.name.escapedText),
+                  undefined
+                )
+              ),
+              factory.createCallSignature(
+                undefined,
+                member.parameters,
+                factory.createTypeReferenceNode(
+                  factory.createIdentifier(node.name.escapedText),
+                  undefined
+                )
+              )
+            ]),
+          // static properties
+          ...node.members
+            .filter(member => member.modifiers?.some?.(modifier => modifier.kind === ts.SyntaxKind.StaticKeyword))
+            .filter(member => !member.modifiers?.some?.(modifier => modifier.kind === ts.SyntaxKind.PrivateKeyword))
+            .map(member => declarationToSignature(factory, member))
+          ]
+        ),
+        undefined
+      )],
+      ts.NodeFlags.Const | ts.NodeFlags.Ambient | ts.NodeFlags.ContextFlags
+    )
+  )  
+}
+
+function createInterfaceWithMethods(factory, node) {
+  return factory.createInterfaceDeclaration(
+    undefined,
+    [factory.createModifier(ts.SyntaxKind.DeclareKeyword)],
+    node.name,
+    undefined,
+    node.heritageClauses,
+    // instance properties
+    node.members
+      .filter(member => member.kind !== ts.SyntaxKind.Constructor)
+      .filter(member => !member.modifiers?.some?.(modifier => modifier.kind === ts.SyntaxKind.StaticKeyword))
+      .filter(member => !member.modifiers?.some?.(modifier => modifier.kind === ts.SyntaxKind.PrivateKeyword))
+      .map(member => declarationToSignature(factory, member))
+  );
+}
+
+function declarationToSignature(factory, node) {
+  switch (node.kind) {
+    case ts.SyntaxKind.PropertyDeclaration:
+      return factory.createPropertySignature(
+        undefined,
+        node.name,
+        undefined,
+        node.type
+      );
+    case ts.SyntaxKind.MethodDeclaration:
+      return factory.createMethodSignature(
+        undefined,
+        node.name,
+        undefined,
+        undefined,
+        node.parameters,
+        node.type
+      );
+  }
+  throw new Error(`Could not convert node of kind ${node.kind} to signature.`);
 }
 
 function createNodeWithFactories(factory, node) {
