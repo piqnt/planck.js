3874c8d2dbe9a667fb180956fd7b42ba6eb9fbef
Oliver Zell <zOadT@users.noreply.github.com>
2023-06-11 18:27:41 +0200

    remove workaround for typescript issue

--- build-utils/factoryConstructorTransformer.js
+++ build-utils/factoryConstructorTransformer.js
@@ -101,118 +101,11 @@ function visitor(factory, node, shared) {
   if (!isRelevantClassDeclaration(node, shared)) {
     return node;
   }
-  if (isClassDerived(node)) {
-    // workaround for https://github.com/microsoft/TypeScript/issues/46503
-    return splitClassIntoConstAndInterface(factory, node);
-  }
   return createNodeWithFactories(factory, node);
 }
 
 function isRelevantClassDeclaration(node, shared) {
   return ts.isClassDeclaration(node) && shared.classes.has(node.name.escapedText);
-}
-
-function isClassDerived(node) {
-  return node.heritageClauses[0] !== undefined;
-}
-
-function splitClassIntoConstAndInterface(factory, node) {
-  return [
-    createConstNodeWithStaticMethods(factory, node),
-    createInterfaceWithMethods(factory, node)
-  ];
-}
-
-function createConstNodeWithStaticMethods(factory, node) {
-  return factory.createVariableStatement(
-    [factory.createModifier(ts.SyntaxKind.DeclareKeyword)],
-    factory.createVariableDeclarationList(
-      [factory.createVariableDeclaration(
-        node.name,
-        undefined,
-        factory.createTypeLiteralNode([
-          // constructors
-          ...node.members
-            .filter(ts.isConstructorDeclaration)
-            .flatMap(member => [
-              factory.createConstructSignature(
-                undefined,
-                member.parameters,
-                factory.createTypeReferenceNode(
-                  node.name,
-                  undefined
-                )
-              ),
-              factory.createCallSignature(
-                undefined,
-                member.parameters,
-                factory.createTypeReferenceNode(
-                  node.name,
-                  undefined
-                )
-              )
-            ].map(node => copyComments(node, member))
-          ),
-          // static properties
-          ...node.members
-            .filter(member => hasModifier(ts.SyntaxKind.StaticKeyword, member))
-            .filter(member => !hasModifier(ts.SyntaxKind.PrivateKeyword, member))
-            .map(member => declarationToSignature(factory, member))
-          ]
-        ),
-        undefined
-      )],
-      ts.NodeFlags.Const | ts.NodeFlags.Ambient | ts.NodeFlags.ContextFlags
-    )
-  );
-}
-
-function createInterfaceWithMethods(factory, node) {
-  const result = factory.createInterfaceDeclaration(
-    undefined,
-    [factory.createModifier(ts.SyntaxKind.DeclareKeyword)],
-    node.name,
-    undefined,
-    node.heritageClauses,
-    // instance properties
-    node.members
-      .filter(member => !ts.isConstructorDeclaration(member))
-      .filter(member => !hasModifier(ts.SyntaxKind.StaticKeyword, member))
-      .filter(member => !hasModifier(ts.SyntaxKind.PrivateKeyword, member))
-      .map(member => declarationToSignature(factory, member))
-  );
-  return copyComments(result, node);
-}
-
-function hasModifier(kind, node) {
-  return node.modifiers?.some?.(modifier => modifier.kind === kind);
-}
-
-function declarationToSignature(factory, node) {
-  let result;
-  switch (node.kind) {
-    case ts.SyntaxKind.PropertyDeclaration:
-      result = factory.createPropertySignature(
-        undefined,
-        node.name,
-        undefined,
-        node.type
-      );
-      break;
-    case ts.SyntaxKind.MethodDeclaration:
-      result = factory.createMethodSignature(
-        undefined,
-        node.name,
-        undefined,
-        undefined,
-        node.parameters,
-        node.type
-      );
-      break;
-    default:
-      throw new Error(`Could not convert node of kind ${node.kind} to signature.`);
-  }
-  return copyComments(result, node);
 }
 
 function createNodeWithFactories(factory, node) {
