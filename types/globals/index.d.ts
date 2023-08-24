549e597d11e6ccd8f29e5e3de8aa2abea72349c6
Ali Shakiba <shakiba@users.noreply.github.com>
2023-08-24 12:28:21 -0700

    add template testbed example

--- types/globals/index.d.ts
+++ types/globals/index.d.ts
@@ -1,3 +1,5 @@
 declare const CONSTRUCTOR_FACTORY: boolean | undefined;
 declare const ASSERT: boolean | undefined;
 declare const PLANCK_VERSION: string | undefined;
+
+declare const planck: typeof import("../../src/main");
