5bcff14f72d9cd4043bfd420174f3cee46f9f844
Arsham <arsham.ramezani@gmail.com>
2024-07-22 07:35:12 -0700

    perf: code of polycollision converted to typescript

--- example/PolyCollision.ts
+++ example/PolyCollision.ts
@@ -20,64 +20,80 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+import planck from "../src/main";
 
-const { World, Vec2, Transform, Manifold, CollidePolygons, Box, Testbed } = planck;
+const { World, Vec2, Transform, Manifold, CollidePolygons, Box, Testbed } =
+  planck;
 
-let world = new World(new Vec2(0, -10));
+const world = new World(new Vec2(0, -10));
 
 const testbed = Testbed.mount();
-testbed.info('Use arrow keys to move and Z or X to rotate.');
+testbed.info("Use arrow keys to move and Z or X to rotate.");
 testbed.start(world);
 
-let polygonA = new Box(2, 4);
-let transformA = new Transform(new Vec2(0.0, 0.0), 0.0);
+const polygonA = new Box(2, 4);
+const transformA = new Transform(new Vec2(0.0, 0.0), 0.0);
 
-let polygonB = new Box(5, 5);
-let positionB = new Vec2(5, 4);
+const polygonB = new Box(5, 5);
+const positionB = new Vec2(5, 4);
 let angleB = 1.9160721;
-let transformB = new Transform(positionB, angleB);
+const transformB = new Transform(positionB, angleB);
 
-testbed.step = function() {
-  let manifold = new Manifold();
+testbed.step = function () {
+  const manifold = new Manifold();
   new CollidePolygons(manifold, polygonA, transformA, polygonB, transformB);
 
-  let worldManifold = manifold.getWorldManifold(null, transformA, polygonA.getRadius(), transformB, polygonB.getRadius());
+  const worldManifold = manifold.getWorldManifold(
+    null,
+    transformA,
+    polygonA.getRadius(),
+    transformB,
+    polygonB.getRadius()
+  );
 
-  testbed.status('point count', manifold.pointCount);
+  testbed.status("point count", manifold.pointCount);
 
-  let vA = polygonA.m_vertices.map(v => Transform.mul(transformA, v));
+  const vA = polygonA.m_vertices.map((v: planck.Vec2) =>
+    Transform.mul(transformA, v)
+  );
   testbed.drawPolygon(vA, testbed.color(0.9, 0.9, 0.9));
 
-  let vB = polygonB.m_vertices.map(v => Transform.mul(transformB, v));
+  const vB = polygonB.m_vertices.map((v: planck.Vec2) =>
+    Transform.mul(transformB, v)
+  );
   testbed.drawPolygon(vB, testbed.color(0.9, 0.9, 0.9));
 
   for (let i = 0; i < manifold.pointCount; ++i) {
-    testbed.drawPoint(worldManifold.points[i], 4.0, testbed.color(0.9, 0.3, 0.3));
+    testbed.drawPoint(
+      worldManifold.points[i],
+      4.0,
+      testbed.color(0.9, 0.3, 0.3)
+    );
   }
 };
 
-testbed.keydown = function() {
-  if (testbed.activeKeys['left']) {
+testbed.keydown = function () {
+  if (testbed.activeKeys["left"]) {
     positionB.x -= 0.2;
   }
 
-  if (testbed.activeKeys['right']) {
+  if (testbed.activeKeys["right"]) {
     positionB.x += 0.2;
   }
 
-  if (testbed.activeKeys['down']) {
+  if (testbed.activeKeys["down"]) {
     positionB.y -= 0.2;
   }
 
-  if (testbed.activeKeys['up']) {
+  if (testbed.activeKeys["up"]) {
     positionB.y += 0.2;
   }
 
-  if (testbed.activeKeys['Z']) {
+  if (testbed.activeKeys["Z"]) {
     angleB += 0.2;
   }
 
-  if (testbed.activeKeys['X']) {
+  if (testbed.activeKeys["X"]) {
     angleB -= 0.2;
   }
 
