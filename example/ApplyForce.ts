ca65110a5637e41e1c02ef54fae7e8824786cab3
Arsham <arsham.ramezani@gmail.com>
2024-07-22 05:19:15 -0700

    perf: applyforce  example converted to typescript

--- example/ApplyForce.ts
+++ example/ApplyForce.ts
@@ -21,7 +21,9 @@
  * SOFTWARE.
  */
 
-const { Vec2, Transform, Polygon, Box, FrictionJoint, World, Edge, Testbed } = planck;
+import planck from "../src/main";
+
+const { Transform, Polygon, Box, FrictionJoint, World, Edge, Testbed } = planck;
 
 const world = new World();
 
@@ -29,7 +31,7 @@ const testbed = Testbed.mount();
 testbed.y = -20;
 testbed.start(world);
 
-const ground = world.createBody(new Vec2(0.0, 20.0));
+const ground = world.createBody(new planck.Vec2(0.0, 20.0));
 
 const wallFD = {
   density: 0.0,
@@ -37,42 +39,42 @@ const wallFD = {
 };
 
 // Left vertical
-ground.createFixture(new Edge(new Vec2(-20.0, -20.0), new Vec2(-20.0, 20.0)), wallFD);
+ground.createFixture(new Edge(new planck.Vec2(-20.0, -20.0), new planck.Vec2(-20.0, 20.0)), wallFD);
 
 // Right vertical
-ground.createFixture(new Edge(new Vec2(20.0, -20.0), new Vec2(20.0, 20.0)), wallFD);
+ground.createFixture(new Edge(new planck.Vec2(20.0, -20.0), new planck.Vec2(20.0, 20.0)), wallFD);
 
 // Top horizontal
-ground.createFixture(new Edge(new Vec2(-20.0, 20.0), new Vec2(20.0, 20.0)), wallFD);
+ground.createFixture(new Edge(new planck.Vec2(-20.0, 20.0), new planck.Vec2(20.0, 20.0)), wallFD);
 
 // Bottom horizontal
-ground.createFixture(new Edge(new Vec2(-20.0, -20.0), new Vec2(20.0, -20.0)), wallFD);
+ground.createFixture(new Edge(new planck.Vec2(-20.0, -20.0), new planck.Vec2(20.0, -20.0)), wallFD);
 
 const xf1 = new Transform();
 xf1.q.set(0.3524 * Math.PI);
 xf1.p.set(xf1.q.getXAxis());
 
 const poly1 = new Polygon([
-  new Vec2(-1.0, 0.0),
-  new Vec2(1.0, 0.0),
-  new Vec2(0.0, 0.5)
-].map(v => Transform.mul(xf1, v)));
+  new planck.Vec2(-1.0, 0.0),
+  new planck.Vec2(1.0, 0.0),
+  new planck.Vec2(0.0, 0.5)
+].map((v:planck.Vec2) => Transform.mul(xf1, v)));
 
 const xf2 = new Transform();
 xf2.q.set(-0.3524 * Math.PI);
-xf2.p.set(Vec2.neg(xf2.q.getXAxis()));
+xf2.p.set(planck.Vec2.neg(xf2.q.getXAxis()));
 
 const poly2 = new Polygon([
-  new Vec2(-1.0, 0.0),
-  new Vec2(1.0, 0.0),
-  new Vec2(0.0, 0.5)
-].map(v => Transform.mul(xf2, v)));
+  new planck.Vec2(-1.0, 0.0),
+  new planck.Vec2(1.0, 0.0),
+  new planck.Vec2(0.0, 0.5)
+].map((v:planck.Vec2) => Transform.mul(xf2, v)));
 
 const jet = world.createBody({
   type : 'dynamic',
   angularDamping : 2.0,
   linearDamping : 0.5,
-  position : new Vec2(0.0, 2.0),
+  position : new planck.Vec2(0.0, 2.0),
   angle : Math.PI,
   allowSleep : false
 });
@@ -85,8 +87,8 @@ const boxFD = {
   friction: 0.3,
 };
 
-for (const i = 0; i < 10; ++i) {
-  const box = world.createDynamicBody(new Vec2(0.0, 5.0 + 1.54 * i));
+for (let i = 0; i < 10; ++i) {
+  const box = world.createDynamicBody(new planck.Vec2(0.0, 5.0 + 1.54 * i));
 
   box.createFixture(new Box(0.5, 0.5), boxFD);
 
@@ -101,7 +103,7 @@ for (const i = 0; i < 10; ++i) {
     collideConnected : true,
     maxForce : mass * gravity,
     maxTorque : mass * radius * gravity
-  }, ground, box));
+  }, ground, box,box.getPosition()));
 }
 
 testbed.step = function() {
@@ -113,8 +115,8 @@ testbed.step = function() {
   }
 
   if (testbed.activeKeys.up) {
-    const f = jet.getWorldVector(new Vec2(0.0, -1.0));
-    const p = jet.getWorldPoint(new Vec2(0.0, 2.0));
+    const f = jet.getWorldVector(new planck.Vec2(0.0, -1.0));
+    const p = jet.getWorldPoint(new planck.Vec2(0.0, 2.0));
     jet.applyLinearImpulse(f, p, true);
   }
 };
