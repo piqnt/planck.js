ab65537d12af59f45e4511e1710d69f3377f3e95
Arsham <arsham.ramezani@gmail.com>
2024-07-22 06:25:26 -0700

    perf: code of chain converted to typescript

--- example/Chain.ts
+++ example/Chain.ts
@@ -20,32 +20,40 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+import planck from "../src/main";
 
 const { World, Vec2, Edge, Box, RevoluteJoint, Testbed } = planck;
 
-let world = new World(new Vec2(0, -10));
+const world = new World(new Vec2(0, -10));
 
 const testbed = Testbed.mount();
 testbed.start(world);
 
-let ground = world.createBody();
+const ground = world.createBody();
 ground.createFixture(new Edge(new Vec2(-40.0, 0.0), new Vec2(40.0, 0.0)), 0.0);
 
-let shape = new Box(0.6, 0.125);
+const shape = new Box(0.6, 0.125);
 
-let y = 25.0;
+const y = 25.0;
 let prevBody = ground;
 for (let i = 0; i < 30; ++i) {
-  let body = world.createDynamicBody(new Vec2(0.5 + i, y));
+  const body = world.createDynamicBody(new Vec2(0.5 + i, y));
   body.createFixture(shape, {
     density: 20.0,
     friction: 0.2,
   });
 
-  let anchor = new Vec2(i, y);
-  world.createJoint(new RevoluteJoint({
-    collideConnected: false,
-  }, prevBody, body, anchor));
+  const anchor = new Vec2(i, y);
+  world.createJoint(
+    new RevoluteJoint(
+      {
+        collideConnected: false,
+      },
+      prevBody,
+      body,
+      anchor
+    )
+  );
 
   prevBody = body;
 }
