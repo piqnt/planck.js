ef9c8bd368c552d24d39c85c5ecd5fc031d86b12
Arsham <arsham.ramezani@gmail.com>
2024-07-22 07:36:29 -0700

    perf: code of prismatic converted to typescript

--- example/Prismatic.ts
+++ example/Prismatic.ts
@@ -22,70 +22,73 @@
  */
 
 // The motor in this test gets smoother with higher velocity iterations.
+import planck from "../src/main";
 
 const { World, Vec2, PrismaticJoint, Edge, Box, Testbed } = planck;
 
-let world = new World(new Vec2(0, -10));
+const world = new World(new Vec2(0, -10));
 
 const testbed = Testbed.mount();
 testbed.start(world);
 
-let MOTOR_SPEED = 10;
+const MOTOR_SPEED = 10;
 
-let ground = world.createBody();
+const ground = world.createBody();
 ground.createFixture(new Edge(new Vec2(-40.0, 0.0), new Vec2(40.0, 0.0)), 0.0);
 
-let body = world.createBody({
-  type : 'dynamic',
-  position : new Vec2(-10.0, 10.0),
-  angle : 0.5 * Math.PI,
-  allowSleep : false
+const body = world.createBody({
+  type: "dynamic",
+  position: new Vec2(-10.0, 10.0),
+  angle: 0.5 * Math.PI,
+  allowSleep: false,
 });
 body.createFixture(new Box(2.0, 0.5), 5.0);
 
 // Bouncy limit
-let axis = new Vec2(2.0, 1.0);
+const axis = new Vec2(2.0, 1.0);
 axis.normalize();
-let joint = new PrismaticJoint({
-  motorSpeed : MOTOR_SPEED,
-  maxMotorForce : 10000.0,
-  enableMotor : true,
-  lowerTranslation : 0.0,
-  upperTranslation : 20.0,
-  enableLimit : true
-}, ground, body, new Vec2(0.0, 0.0), axis);
+const joint = new PrismaticJoint(
+  {
+    motorSpeed: MOTOR_SPEED,
+    maxMotorForce: 10000.0,
+    enableMotor: true,
+    lowerTranslation: 0.0,
+    upperTranslation: 20.0,
+    enableLimit: true,
+  },
+  ground,
+  body,
+  new Vec2(0.0, 0.0),
+  axis
+);
 
 // Non-bouncy limit
 // (ground, body, new Vec2(-10.0, 10.0), new Vec2(1.0, 0.0));
 
 world.createJoint(joint);
 
-testbed.step = function() {
+testbed.step = function () {
   if (testbed.activeKeys.right && !testbed.activeKeys.left) {
     joint.enableLimit(true);
     joint.enableMotor(true);
     joint.setMotorSpeed(+MOTOR_SPEED);
-
   } else if (testbed.activeKeys.left && !testbed.activeKeys.right) {
     joint.enableLimit(true);
     joint.enableMotor(true);
     joint.setMotorSpeed(-MOTOR_SPEED);
-
   } else if (testbed.activeKeys.up && !testbed.activeKeys.down) {
     joint.enableLimit(false);
     joint.enableMotor(true);
     joint.setMotorSpeed(+MOTOR_SPEED);
-
   } else if (testbed.activeKeys.down && !testbed.activeKeys.up) {
     joint.enableLimit(false);
     joint.enableMotor(true);
     joint.setMotorSpeed(-MOTOR_SPEED);
-
   } else {
     joint.enableLimit(true);
     joint.enableMotor(false);
   }
 
-  let force = joint.getMotorForce(1 / 60);
-  testbed.status('Motor Force', force);
+  const force = joint.getMotorForce(1 / 60);
+  testbed.status("Motor Force", force);
 };
