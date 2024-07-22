8a2d1cb5384f0ac4d05c5f5a2f996fb4649c4f30
Arsham <arsham.ramezani@gmail.com>
2024-07-22 05:54:02 -0700

    perf: code of breakable converted to typescript

--- example/Breakable.ts
+++ example/Breakable.ts
@@ -22,40 +22,41 @@
  */
 
 // This is used to test sensor shapes.
+import planck from "../src/main";
 
 const { World, Vec2, Edge, Box, Testbed } = planck;
 
-let world = new World(new Vec2(0, -10));
+const world = new World(new Vec2(0, -10));
 
 const testbed = Testbed.mount();
 testbed.start(world);
 
-let breakVelocity;
-let breakAngularVelocity;
+let breakVelocity: planck.Vec2;
+let breakAngularVelocity: number;
 
 let broke = false;
 
 // Ground body
-let ground = world.createBody();
+const ground = world.createBody();
 ground.createFixture(new Edge(new Vec2(-40.0, 0.0), new Vec2(40.0, 0.0)), 0.0);
 
 // Breakable dynamic body
-let body1 = world.createDynamicBody(new Vec2(0.0, 40.0), 0.25 * Math.PI);
+const body1 = world.createDynamicBody(new Vec2(0.0, 40.0), 0.25 * Math.PI);
 
-let shape1 = new Box(0.5, 0.5, new Vec2(-0.5, 0.0), 0.0);
-let piece1 = body1.createFixture(shape1, 1.0);
+const shape1 = new Box(0.5, 0.5, new Vec2(-0.5, 0.0), 0.0);
+const piece1 = body1.createFixture(shape1, 1.0);
 
-let shape2 = new Box(0.5, 0.5, new Vec2(0.5, 0.0), 0.0);
+const shape2 = new Box(0.5, 0.5, new Vec2(0.5, 0.0), 0.0);
 let piece2 = body1.createFixture(shape2, 1.0);
 
-world.on('post-solve', function (contact, impulse) {
+world.on("post-solve", function (contact, impulse) {
   if (broke) {
     // The body already broke.
     return;
   }
 
   // Should the body break?
-  let count = contact.getManifold().pointCount;
+  const count = contact.getManifold().pointCount;
 
   let maxImpulse = 0.0;
   for (let i = 0; i < count; ++i) {
@@ -63,7 +64,7 @@ world.on('post-solve', function (contact, impulse) {
   }
 
   if (maxImpulse > 40.0) {
-    setTimeout(function() {
+    setTimeout(function () {
       Break();
       broke = true;
     });
@@ -72,21 +73,27 @@ world.on('post-solve', function (contact, impulse) {
 
 function Break() {
   // Create two bodies from one.
-  let center = body1.getWorldCenter();
+  const center = body1.getWorldCenter();
 
   body1.destroyFixture(piece2);
 
-  let body2 = world.createDynamicBody(body1.getPosition(), body1.getAngle());
+  const body2 = world.createDynamicBody(body1.getPosition(), body1.getAngle());
 
   piece2 = body2.createFixture(shape2, 1.0);
 
   // Compute consistent velocities for new bodies based on
   // cached velocity.
-  let center1 = body1.getWorldCenter();
-  let center2 = body2.getWorldCenter();
+  const center1 = body1.getWorldCenter();
+  const center2 = body2.getWorldCenter();
 
-  let velocity1 = Vec2.add(breakVelocity, Vec2.cross(breakAngularVelocity, Vec2.sub(center1, center)));
-  let velocity2 = Vec2.add(breakVelocity, Vec2.cross(breakAngularVelocity, Vec2.sub(center2, center)));
+  const velocity1 = Vec2.add(
+    breakVelocity,
+    Vec2.cross(breakAngularVelocity, Vec2.sub(center1, center))
+  );
+  const velocity2 = Vec2.add(
+    breakVelocity,
+    Vec2.cross(breakAngularVelocity, Vec2.sub(center2, center))
+  );
 
   body1.setAngularVelocity(breakAngularVelocity);
   body1.setLinearVelocity(velocity1);
@@ -95,7 +102,7 @@ function Break() {
   body2.setLinearVelocity(velocity2);
 }
 
-testbed.step = function() {
+testbed.step = function () {
   // Cache velocities to improve movement on breakage.
   if (!broke) {
     breakVelocity = body1.getLinearVelocity();
