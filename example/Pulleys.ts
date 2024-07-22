fe4d9c6821a57d9ae2c8d15214f4e6356d6b9f76
Arsham <arsham.ramezani@gmail.com>
2024-07-22 07:36:54 -0700

    perf: code of pulleys converted to typescript

--- example/Pulleys.ts
+++ example/Pulleys.ts
@@ -21,44 +21,57 @@
  * SOFTWARE.
  */
 
+import planck from "../src/main";
+
 const { Vec2, World, Circle, Box, PulleyJoint, Testbed } = planck;
 
-let world = new World(new Vec2(0, -10));
+const world = new World(new Vec2(0, -10));
 
 const testbed = Testbed.mount();
 testbed.start(world);
 
-let y = 16.0;
-let L = 12.0;
-let a = 1.0;
-let b = 2.0;
+const y = 16.0;
+const L = 12.0;
+const a = 1.0;
+const b = 2.0;
 
-let ground = world.createBody();
+const ground = world.createBody();
 
 // ground.createFixture(new Edge(new Vec2(-40.0, 0.0), new Vec2(40.0, 0.0)), 0.0);
 
 ground.createFixture(new Circle(new Vec2(-10.0, y + b + L), 2.0), 0.0);
 ground.createFixture(new Circle(new Vec2(10.0, y + b + L), 2.0), 0.0);
 
-let shape = new Box(a, b);
+const shape = new Box(a, b);
 
 // bd.fixedRotation = true;
-let box1 = world.createDynamicBody(new Vec2(-10.0, y));
+const box1 = world.createDynamicBody(new Vec2(-10.0, y));
 box1.createFixture(shape, 5.0);
 
-let box2 = world.createDynamicBody(new Vec2(10.0, y));
+const box2 = world.createDynamicBody(new Vec2(10.0, y));
 box2.createFixture(shape, 5.0);
 
-let anchor1 = new Vec2(-10.0, y + b);
-let anchor2 = new Vec2(10.0, y + b);
-let groundAnchor1 = new Vec2(-10.0, y + b + L);
-let groundAnchor2 = new Vec2(10.0, y + b + L);
+const anchor1 = new Vec2(-10.0, y + b);
+const anchor2 = new Vec2(10.0, y + b);
+const groundAnchor1 = new Vec2(-10.0, y + b + L);
+const groundAnchor2 = new Vec2(10.0, y + b + L);
 
-let joint1 = world.createJoint(new PulleyJoint({}, box1, box2, groundAnchor1, groundAnchor2, anchor1, anchor2, 1.5));
+const joint1 = world.createJoint(
+  new PulleyJoint(
+    {},
+    box1,
+    box2,
+    groundAnchor1,
+    groundAnchor2,
+    anchor1,
+    anchor2,
+    1.5
+  )
+);
 
-testbed.step = function() {
-  let ratio = joint1.getRatio();
-  let L = joint1.getCurrentLengthA() + ratio * joint1.getCurrentLengthB();
-  testbed.status('ratio', ratio);
-  testbed.status('L (L1 * ratio + L2)', L);
+testbed.step = function () {
+  const ratio = joint1.getRatio();
+  const L = joint1.getCurrentLengthA() + ratio * joint1.getCurrentLengthB();
+  testbed.status("ratio", ratio);
+  testbed.status("L (L1 * ratio + L2)", L);
 };
