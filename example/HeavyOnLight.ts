8a1cfb12e0678b3f2340537c6d6946e1668e1a1b
Arsham <arsham.ramezani@gmail.com>
2024-07-22 07:29:40 -0700

    perf: code of heavyonlight converted to typescript

--- example/HeavyOnLight.ts
+++ example/HeavyOnLight.ts
@@ -21,15 +21,23 @@
  * SOFTWARE.
  */
 
+import planck from "../src/main";
+
 const { Vec2, World, Edge, Circle, Testbed } = planck;
 
-let world = new World(new Vec2(0, -10));
+const world = new World(new Vec2(0, -10));
 
 const testbed = Testbed.mount();
 testbed.start(world);
 
-world.createBody().createFixture(new Edge(new Vec2(-40.0, 0.0), new Vec2(40.0, 0.0)));
+world
+  .createBody()
+  .createFixture(new Edge(new Vec2(-40.0, 0.0), new Vec2(40.0, 0.0)));
 
-world.createDynamicBody(new Vec2(0.0, 4.5)).createFixture(new Circle(0.5), 10.0);
+world
+  .createDynamicBody(new Vec2(0.0, 4.5))
+  .createFixture(new Circle(0.5), 10.0);
 
-world.createDynamicBody(new Vec2(0.0, 10.0)).createFixture(new Circle(5.0), 10.0);
+world
+  .createDynamicBody(new Vec2(0.0, 10.0))
+  .createFixture(new Circle(5.0), 10.0);
