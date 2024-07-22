4b02171cbd3b689aec81d513ad6eb1fa074ba315
Arsham <arsham.ramezani@gmail.com>
2024-07-22 07:30:21 -0700

    perf: code of heavyonlighttwo converted to typescript

--- example/HeavyOnLightTwo.ts
+++ example/HeavyOnLightTwo.ts
@@ -21,21 +21,29 @@
  * SOFTWARE.
  */
 
-let { World, Vec2, Circle, Edge, Testbed } = planck;
+import planck from "../src/main";
 
-let world = new World(new Vec2(0, -10));
+const { World, Vec2, Circle, Edge, Testbed } = planck;
+
+const world = new World(new Vec2(0, -10));
 
 const testbed = Testbed.mount();
-testbed.info('X: Add/Remove heavy circle');
+testbed.info("X: Add/Remove heavy circle");
 testbed.start(world);
 
-world.createBody().createFixture(new Edge(new Vec2(-40.0, 0.0), new Vec2(40.0, 0.0)), 0.0);
+world
+  .createBody()
+  .createFixture(new Edge(new Vec2(-40.0, 0.0), new Vec2(40.0, 0.0)), 0.0);
 
-world.createDynamicBody(new Vec2(0.0, 2.5)).createFixture(new Circle(0.5), 10.0);
+world
+  .createDynamicBody(new Vec2(0.0, 2.5))
+  .createFixture(new Circle(0.5), 10.0);
 
-world.createDynamicBody(new Vec2(0.0, 3.5)).createFixture(new Circle(0.5), 10.0);
+world
+  .createDynamicBody(new Vec2(0.0, 3.5))
+  .createFixture(new Circle(0.5), 10.0);
 
-let heavy = null;
+let heavy: planck.Body = null;
 
 function toggleHeavy() {
   if (heavy) {
@@ -47,10 +55,10 @@ function toggleHeavy() {
   }
 }
 
-testbed.keydown = function(code, char) {
+testbed.keydown = function (code, char) {
   switch (char) {
-  case 'X':
-    toggleHeavy();
-    break;
+    case "X":
+      toggleHeavy();
+      break;
   }
 };
