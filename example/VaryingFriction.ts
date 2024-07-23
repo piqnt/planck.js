d81cd48aac0a353324fb6ccb158c21f860e6bfea
Arsham <arsham.ramezani@gmail.com>
2024-07-23 00:27:50 -0700

    perf: code of varyingfriction converted to typescript

--- example/VaryingFriction.ts
+++ example/VaryingFriction.ts
@@ -21,26 +21,36 @@
  * SOFTWARE.
  */
 
+import planck from "../src/main";
+
 const { World, Vec2, Edge, Box, Testbed } = planck;
 
-let world = new World(new Vec2(0, -10));
+const world = new World(new Vec2(0, -10));
 
 const testbed = Testbed.mount();
 testbed.start(world);
 
-world.createBody().createFixture(new Edge(new Vec2(-40.0, 0.0), new Vec2(40.0, 0.0)));
+world
+  .createBody()
+  .createFixture(new Edge(new Vec2(-40.0, 0.0), new Vec2(40.0, 0.0)));
 
-world.createBody(new Vec2(-4.0, 22.0), -0.25).createFixture(new Box(13.0, 0.25), 0.0);
+world
+  .createBody(new Vec2(-4.0, 22.0), -0.25)
+  .createFixture(new Box(13.0, 0.25), 0.0);
 
 world.createBody(new Vec2(10.5, 19.0)).createFixture(new Box(0.25, 1.0), 0.0);
 
-world.createBody(new Vec2(4.0, 14.0), 0.25).createFixture(new Box(13.0, 0.25), 0.0);
+world
+  .createBody(new Vec2(4.0, 14.0), 0.25)
+  .createFixture(new Box(13.0, 0.25), 0.0);
 
 world.createBody(new Vec2(-10.5, 11.0)).createFixture(new Box(0.25, 1.0), 0.0);
 
-world.createBody(new Vec2(-4.0, 6.0), -0.25).createFixture(new Box(13.0, 0.25), 0.0);
+world
+  .createBody(new Vec2(-4.0, 6.0), -0.25)
+  .createFixture(new Box(13.0, 0.25), 0.0);
 
-const friction = [ 0.75, 0.5, 0.35, 0.1, 0.0 ];
+const friction = [0.75, 0.5, 0.35, 0.1, 0.0];
 
 const circle = new Box(0.5, 0.5);
 
@@ -48,6 +58,6 @@ for (let i = 0; i < friction.length; ++i) {
   const ball = world.createDynamicBody(new Vec2(-15.0 + 4.0 * i, 28.0));
   ball.createFixture(circle, {
     density: 25.0,
-    friction: friction[i]
+    friction: friction[i],
   });
 }
