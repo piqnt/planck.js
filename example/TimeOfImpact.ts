f874d719e436f13bba8555f32c961aa19c94c859
Arsham <arsham.ramezani@gmail.com>
2024-07-23 00:26:44 -0700

    perf: code of timeofimpact converted to typescript

--- example/TimeOfImpact.ts
+++ example/TimeOfImpact.ts
@@ -20,10 +20,21 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+import planck from "../src/main";
 
-const { World, Transform, Box, TimeOfImpact, Sweep, TOIInput, TOIOutput, stats, Testbed } = planck;
+const {
+  World,
+  Transform,
+  Box,
+  TimeOfImpact,
+  Sweep,
+  TOIInput,
+  TOIOutput,
+  stats,
+  Testbed,
+} = planck;
 
-let world = new World();
+const world = new World();
 
 const testbed = Testbed.mount();
 testbed.width = 80;
@@ -32,16 +43,16 @@ testbed.x = 0;
 testbed.y = 0;
 testbed.start(world);
 
-let shapeA = new Box(25.0, 5.0);
-let sweepA = new Sweep();
+const shapeA = new Box(25.0, 5.0);
+const sweepA = new Sweep();
 sweepA.c0.set(0, 0);
 sweepA.a0 = 0.1;
 sweepA.c.set(sweepA.c0);
 sweepA.a = sweepA.a0;
 sweepA.localCenter.setZero();
 
-let shapeB = new Box(2.5, 2.5);
-let sweepB = new Sweep();
+const shapeB = new Box(2.5, 2.5);
+const sweepB = new Sweep();
 sweepB.c0.set(20, 20);
 sweepB.a0 = 0.1; // - 162.0 * Math.PI;
 sweepB.c.set(-20, -20);
@@ -51,47 +62,57 @@ sweepB.localCenter.setZero();
 // sweepB.a0 -= 300.0 * Math.PI;
 // sweepB.a -= 300.0 * Math.PI;
 
-let input = new TOIInput();
+const input = new TOIInput();
 input.proxyA.set(shapeA, 0);
 input.sweepA.set(sweepA);
 input.proxyB.set(shapeB, 0);
 input.sweepB.set(sweepB);
 input.tMax = 1.0;
 
-let output = new TOIOutput();
+const output = new TOIOutput();
 
 TimeOfImpact(output, input);
 
-testbed.step = function() {
+testbed.step = function () {
   // "max toi iters = %d, max root iters = %d", b2_toiMaxIters, b2_toiMaxRootIters
 
-  testbed.status('toi', output.t);
+  testbed.status("toi", output.t);
   testbed.status(stats);
 
   let vertices = [];
 
-  let transformB = new Transform();
+  const transformB = new Transform();
 
   for (let t = 0.1; t < 1.0; t += 0.1) {
     sweepB.getTransform(transformB, t);
-    vertices = shapeB.m_vertices.map(v => Transform.mul(transformB, v));
+    vertices = shapeB.m_vertices.map((v: planck.Vec2) =>
+      Transform.mul(transformB, v)
+    );
     testbed.drawPolygon(vertices, testbed.color(0.2, 0.2, 0.2));
   }
 
-  let transformA = new Transform();
+  const transformA = new Transform();
   sweepA.getTransform(transformA, 0.0);
-  vertices = shapeA.m_vertices.map(v => Transform.mul(transformA, v));
+  vertices = shapeA.m_vertices.map((v: planck.Vec2) =>
+    Transform.mul(transformA, v)
+  );
   testbed.drawPolygon(vertices, testbed.color(0.7, 0.7, 0.7));
 
   sweepB.getTransform(transformB, 0.0);
-  vertices = shapeB.m_vertices.map(v => Transform.mul(transformB, v));
+  vertices = shapeB.m_vertices.map((v: planck.Vec2) =>
+    Transform.mul(transformB, v)
+  );
   testbed.drawPolygon(vertices, testbed.color(1, 1, 1));
 
   sweepB.getTransform(transformB, output.t);
-  vertices = shapeB.m_vertices.map(v => Transform.mul(transformB, v));
+  vertices = shapeB.m_vertices.map((v: planck.Vec2) =>
+    Transform.mul(transformB, v)
+  );
   testbed.drawPolygon(vertices, testbed.color(1, 0, 0));
 
   sweepB.getTransform(transformB, 1.0);
-  vertices = shapeB.m_vertices.map(v => Transform.mul(transformB, v));
+  vertices = shapeB.m_vertices.map((v: planck.Vec2) =>
+    Transform.mul(transformB, v)
+  );
   testbed.drawPolygon(vertices, testbed.color(1, 1, 1));
 };
