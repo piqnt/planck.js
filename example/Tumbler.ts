106738777aabfcfe7f28b2e2f1c3624672c74614
Arsham <arsham.ramezani@gmail.com>
2024-07-23 00:27:20 -0700

    perf: code of tumbler converted to typescript

--- example/Tumbler.ts
+++ example/Tumbler.ts
@@ -21,20 +21,22 @@
  * SOFTWARE.
  */
 
+import planck from "../src/main";
+
 const { World, Vec2, Box, RevoluteJoint, Testbed } = planck;
 
-let world = new World(new Vec2(0, -10));
+const world = new World(new Vec2(0, -10));
 
 const testbed = Testbed.mount();
 testbed.start(world);
 
-let COUNT = 200;
+const COUNT = 200;
 
-let ground = world.createBody();
+const ground = world.createBody();
 
-let container = world.createDynamicBody({
+const container = world.createDynamicBody({
   allowSleep: false,
-  position: new Vec2(0, 10)
+  position: new Vec2(0, 10),
 });
 
 container.createFixture(new Box(0.5, 20, new Vec2(20, 0), 0), 5);
@@ -42,17 +44,26 @@ container.createFixture(new Box(0.5, 20, new Vec2(-20, 0), 0), 5);
 container.createFixture(new Box(20, 0.5, new Vec2(0, 20), 0), 5);
 container.createFixture(new Box(20, 0.5, new Vec2(0, -20), 0), 5);
 
-world.createJoint(new RevoluteJoint({
-  motorSpeed: 0.08 * Math.PI,
-  maxMotorTorque: 1e8,
-  enableMotor: true,
-}, ground, container, new Vec2(0, 10)));
+world.createJoint(
+  new RevoluteJoint(
+    {
+      motorSpeed: 0.08 * Math.PI,
+      maxMotorTorque: 1e8,
+      enableMotor: true,
+    },
+    ground,
+    container,
+    new Vec2(0, 10)
+  )
+);
 
-let shape = new Box(0.5, 0.5);
+const shape = new Box(0.5, 0.5);
 let count = 0;
 while (count < COUNT) {
-  let body = world.createDynamicBody();
-  body.setPosition(new Vec2(Math.random() * 20 - 10, 10 + Math.random() * 20 - 10));
+  const body = world.createDynamicBody();
+  body.setPosition(
+    new Vec2(Math.random() * 20 - 10, 10 + Math.random() * 20 - 10)
+  );
   body.createFixture(shape, 1);
   ++count;
 }
