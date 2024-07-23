7e151cb5ca1dc5b333be206b2fc44e2f22ab897b
Arsham <arsham.ramezani@gmail.com>
2024-07-23 00:44:39 -0700

    perf: code of web distancetest and collisionfiltering to typescript

--- example/CollisionFiltering.ts
+++ example/CollisionFiltering.ts
@@ -27,28 +27,32 @@
 // The 3 small ones always collide.
 // The 3 large ones never collide.
 // The boxes don't collide with triangles (except if both are small).
+import planck from "../src/main";
 
-const { World, Vec2, Edge, Polygon, Box, Circle, PrismaticJoint, Testbed } = planck;
+const { World, Vec2, Edge, Polygon, Box, Circle, PrismaticJoint, Testbed } =
+  planck;
 
-let SMALL_GROUP = 1;
-let LARGE_GROUP = -1;
+const SMALL_GROUP = 1;
+const LARGE_GROUP = -1;
 
-let TRIANGLE_CATEGORY = 0x0002;
-let BOX_Category = 0x0004;
-let CIRCLE_CATEGORY = 0x0008;
+const TRIANGLE_CATEGORY = 0x0002;
+const BOX_Category = 0x0004;
+const CIRCLE_CATEGORY = 0x0008;
 
-let TRIANGLE_MASK = 0xFFFF;
-let BOX_MASK = 0xFFFF ^ TRIANGLE_CATEGORY;
-let CIRCLE_MAX = 0xFFFF;
+const TRIANGLE_MASK = 0xffff;
+const BOX_MASK = 0xffff ^ TRIANGLE_CATEGORY;
+const CIRCLE_MAX = 0xffff;
 
-let world = new World(new Vec2(0, -10));
+const world = new World(new Vec2(0, -10));
 
 const testbed = Testbed.mount();
 testbed.start(world);
 
 // Ground body
-let ground = world.createBody();
-ground.createFixture(new Edge(new Vec2(-40.0, 0.0), new Vec2(40.0, 0.0)), {friction : 0.3});
+const ground = world.createBody();
+ground.createFixture(new Edge(new Vec2(-40.0, 0.0), new Vec2(40.0, 0.0)), {
+  friction: 0.3,
+});
 
 // Small triangle
 const smallTriangle = {
@@ -58,15 +62,14 @@ const smallTriangle = {
   filterGroupIndex: SMALL_GROUP,
 };
 
-let body1 = world.createBody({
-  type : 'dynamic',
-  position : new Vec2(-5.0, 2.0)
+const body1 = world.createBody({
+  type: "dynamic",
+  position: new Vec2(-5.0, 2.0),
 });
-body1.createFixture(new Polygon([
-  new Vec2(-1.0, 0.0),
-  new Vec2(1.0, 0.0),
-  new Vec2(0.0, 2.0)
-]), smallTriangle);
+body1.createFixture(
+  new Polygon([new Vec2(-1.0, 0.0), new Vec2(1.0, 0.0), new Vec2(0.0, 2.0)]),
+  smallTriangle
+);
 
 // Large triangle (recycle definitions)
 const largeTriangle = {
@@ -76,71 +79,77 @@ const largeTriangle = {
   filterGroupIndex: LARGE_GROUP,
 };
 
-let body2 = world.createBody({
-  type : 'dynamic',
-  position : new Vec2(-5.0, 6.0),
-  fixedRotation : true // look at me!
+const body2 = world.createBody({
+  type: "dynamic",
+  position: new Vec2(-5.0, 6.0),
+  fixedRotation: true, // look at me!
 });
-body2.createFixture(new Polygon([
-  new Vec2(-2.0, 0.0),
-  new Vec2(2.0, 0.0),
-  new Vec2(0.0, 4.0)
-]), largeTriangle);
+body2.createFixture(
+  new Polygon([new Vec2(-2.0, 0.0), new Vec2(2.0, 0.0), new Vec2(0.0, 4.0)]),
+  largeTriangle
+);
 
-let body = world.createDynamicBody(new Vec2(-5.0, 10.0));
+const body = world.createDynamicBody(new Vec2(-5.0, 10.0));
 body.createFixture(new Box(0.5, 1.0), 1.0);
 
-world.createJoint(new PrismaticJoint({
-  enableLimit : true,
-  localAnchorA : new Vec2(0.0, 4.0),
-  localAnchorB : new Vec2(),
-  localAxisA : new Vec2(0.0, 1.0),
-  lowerTranslation : -1.0,
-  upperTranslation : 1.0
-}, body2, body));
+world.createJoint(
+  /// NEED TO FIX
+  new PrismaticJoint(
+    {
+      enableLimit: true,
+      localAnchorA: new Vec2(0.0, 4.0),
+      localAnchorB: new Vec2(),
+      localAxisA: new Vec2(0.0, 1.0),
+      lowerTranslation: -1.0,
+      upperTranslation: 1.0,
+    },
+    body2,
+    body
+  )
+);
 
 // Small box
 const smallBox = {
-  density : 1.0,
-  restitution : 0.1,
+  density: 1.0,
+  restitution: 0.1,
   filterCategoryBits: BOX_Category,
   filterMaskBits: BOX_MASK,
   filterGroupIndex: SMALL_GROUP,
 };
 
-let body3 = world.createDynamicBody(new Vec2(0.0, 2.0));
+const body3 = world.createDynamicBody(new Vec2(0.0, 2.0));
 body3.createFixture(new Box(1.0, 0.5), smallBox);
 
 // Large box (recycle definitions)
 const largeBox = {
-  density : 1.0,
-  restitution : 0.1,
+  density: 1.0,
+  restitution: 0.1,
   filterCategoryBits: BOX_Category,
   filterMaskBits: BOX_MASK,
   filterGroupIndex: LARGE_GROUP,
 };
 
-let body4 = world.createDynamicBody(new Vec2(0.0, 6.0));
+const body4 = world.createDynamicBody(new Vec2(0.0, 6.0));
 body4.createFixture(new Box(2.0, 1.0), largeBox);
 
 // Small circle
 const smallCircle = {
-  density : 1.0,
+  density: 1.0,
   filterCategoryBits: CIRCLE_CATEGORY,
   filterMaskBits: CIRCLE_MAX,
   filterGroupIndex: SMALL_GROUP,
 };
 
-let body5 = world.createDynamicBody(new Vec2(5.0, 2.0));
+const body5 = world.createDynamicBody(new Vec2(5.0, 2.0));
 body5.createFixture(new Circle(1.0), smallCircle);
 
 // Large circle
 const largeCircle = {
-  density : 1.0,
+  density: 1.0,
   filterCategoryBits: CIRCLE_CATEGORY,
   filterMaskBits: CIRCLE_MAX,
   filterGroupIndex: LARGE_GROUP,
 };
 
-let body6 = world.createDynamicBody(new Vec2(5.0, 6.0));
+const body6 = world.createDynamicBody(new Vec2(5.0, 6.0));
 body6.createFixture(new Circle(2.0), largeCircle);
--- example/DistanceTest.ts
+++ example/DistanceTest.ts
@@ -21,82 +21,85 @@
  * SOFTWARE.
  */
 
+import planck from "../src/main";
+
 const { World, Vec2, Transform, Box, Distance, Testbed } = planck;
 
-let DistanceInput = Distance.Input;
-let DistanceOutput = Distance.Output;
-let SimplexCache = Distance.Cache;
+const DistanceInput = Distance.Input;
+const DistanceOutput = Distance.Output;
+const SimplexCache = Distance.Cache;
 
-let world = new World();
+const world = new World();
 
 const testbed = Testbed.mount();
-testbed.start(world);    
+testbed.start(world);
 
-let transformA = new Transform(new Vec2(0.0, -0.2));
+const transformA = new Transform(new Vec2(0.0, -0.2));
 
-let polygonA = new Box(10.0, 0.2);
+const polygonA = new Box(10.0, 0.2);
 
-let positionB = new Vec2(12.017401, 0.13678508);
+const positionB = new Vec2(12.017401, 0.13678508);
 let angleB = -0.0109265;
-let transformB = new Transform(positionB, angleB);
+const transformB = new Transform(positionB, angleB);
 
-let polygonB = new Box(2.0, 0.1);
+const polygonB = new Box(2.0, 0.1);
 
-let bodyA = world.createBody();
-let fixA = bodyA.createFixture(polygonA);
+const bodyA = world.createBody();
+const fixA = bodyA.createFixture(polygonA);
 
-let bodyB = world.createBody();
-let fixB = bodyB.createFixture(polygonB);
+const bodyB = world.createBody();
+const fixB = bodyB.createFixture(polygonB);
 
-testbed.step = function() {
-  let input = new DistanceInput();
+testbed.step = function () {
+  const input = new DistanceInput();
   input.proxyA.set(polygonA, 0);
   input.proxyB.set(polygonB, 0);
   input.transformA.set(transformA);
   input.transformB.set(transformB);
   input.useRadii = true;
 
-  let cache = new SimplexCache();
+  const cache = new SimplexCache();
 
-  let output = new DistanceOutput();
+  const output = new DistanceOutput();
 
   Distance(output, cache, input);
 
-  testbed.status('Distance', output.distance);
-  testbed.status('Iterations', output.iterations);
+  testbed.status("Distance", output.distance);
+  testbed.status("Iterations", output.iterations);
 
+  /// NEED TO FIX THIS
   bodyA.setTransform(transformA);
   bodyB.setTransform(transformB);
 
-  let x1 = output.pointA;
-  let x2 = output.pointB;
+  const x1 = output.pointA;
+  const x2 = output.pointB;
 
   testbed.drawPoint(x1, 4.0, testbed.color(1.0, 0.0, 0.0));
   testbed.drawPoint(x2, 4.0, testbed.color(1.0, 1.0, 0.0));
 };
 
-testbed.keydown = function() {
-  if (testbed.activeKeys['left']) {
+testbed.keydown = function () {
+  if (testbed.activeKeys["left"]) {
     positionB.x -= 0.1;
   }
 
-  if (testbed.activeKeys['right']) {
+  if (testbed.activeKeys["right"]) {
     positionB.x += 0.1;
   }
 
-  if (testbed.activeKeys['down']) {
+  if (testbed.activeKeys["down"]) {
     positionB.y -= 0.1;
   }
 
-  if (testbed.activeKeys['up']) {
+  if (testbed.activeKeys["up"]) {
     positionB.y += 0.1;
   }
 
-  if (testbed.activeKeys['Z']) {
+  if (testbed.activeKeys["Z"]) {
     angleB += 0.1;
   }
 
-  if (testbed.activeKeys['X']) {
+  if (testbed.activeKeys["X"]) {
     angleB -= 0.1;
   }
 
