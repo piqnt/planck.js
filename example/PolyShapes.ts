5623e92c5b47d5f7dfdc51259bf078bfd494fb5c
Arsham <arsham.ramezani@gmail.com>
2024-07-23 00:57:13 -0700

    perf: code of polyshapes converted to typescript

--- example/PolyShapes.ts
+++ example/PolyShapes.ts
@@ -20,38 +20,50 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+import planck from "../src/main";
 
-const { Vec2, Transform, AABB, Circle, Polygon, Edge, Box, World, Distance, Testbed } = planck;
+const {
+  Vec2,
+  Transform,
+  AABB,
+  Circle,
+  Polygon,
+  Edge,
+  Box,
+  World,
+  Distance,
+  Testbed,
+} = planck;
 
-let world = new World(new Vec2(0, -10));
+const world = new World(new Vec2(0, -10));
 const testbed = Testbed.mount();
 testbed.start(world);
 
-let MAX_BODIES = 256;
+const MAX_BODIES = 256;
 
-let bodies = [];
+const bodies = [];
 
-let shapes = [];
+const shapes = [];
 
-let ground = world.createBody();
+const ground = world.createBody();
 ground.createFixture(new Edge(new Vec2(-40.0, 0.0), new Vec2(40.0, 0.0)), 0.0);
 
 shapes[0] = new Polygon([
   new Vec2(-0.5, 0.0),
   new Vec2(0.5, 0.0),
-  new Vec2(0.0, 1.5)
+  new Vec2(0.0, 1.5),
 ]);
 
 shapes[1] = new Polygon([
   new Vec2(-0.1, 0.0),
   new Vec2(0.1, 0.0),
-  new Vec2(0.0, 1.5)
+  new Vec2(0.0, 1.5),
 ]);
 
 {
-  let w = 1.0;
-  let b = w / (2.0 + Math.sqrt(2.0));
-  let s = Math.sqrt(2.0) * b;
+  const w = 1.0;
+  const b = w / (2.0 + Math.sqrt(2.0));
+  const s = Math.sqrt(2.0) * b;
 
   shapes[2] = new Polygon([
     new Vec2(0.5 * s, 0.0),
@@ -74,8 +86,8 @@ function createBody(index) {
     world.destroyBody(bodies.shift());
   }
 
-  let bd = {
-    type: 'dynamic',
+  const bd: any = {
+    type: "dynamic",
     position: new Vec2(Math.random() * 0.4 - 2.0, 10.0),
     angle: Math.random() * 2 * Math.PI - Math.PI,
   };
@@ -84,7 +96,7 @@ function createBody(index) {
     bd.angularDamping = 0.02;
   }
 
-  let body = world.createBody(bd);
+  const body = world.createBody(bd);
 
   body.createFixture(shapes[index % shapes.length], {
     density: 1.0,
@@ -98,79 +110,89 @@ function destroyBody() {
   world.destroyBody(bodies.shift());
 }
 
-testbed.keydown = function(code, char) {
+testbed.keydown = function (code, char) {
   switch (char) {
-  case '1':
-    createBody(1);
-    break;
+    case "1":
+      createBody(1);
+      break;
 
-  case '2':
-    createBody(2);
-    break;
+    case "2":
+      createBody(2);
+      break;
 
-  case '3':
-    createBody(3);
-    break;
+    case "3":
+      createBody(3);
+      break;
 
-  case '4':
-    createBody(4);
-    break;
+    case "4":
+      createBody(4);
+      break;
 
-  case '5':
-    createBody(5);
-    break;
+    case "5":
+      createBody(5);
+      break;
 
-  case 'Z':
-    for (let i = 0; i < bodies.length; i += 2) {
-      let body = bodies[i];
-      body.setActive(!body.isActive());
-    }
-    break;
+    case "Z":
+      for (let i = 0; i < bodies.length; i += 2) {
+        const body = bodies[i];
+        body.setActive(!body.isActive());
+      }
+      break;
 
-  case 'X':
-    destroyBody();
-    break;
+    case "X":
+      destroyBody();
+      break;
   }
 };
 
-testbed.info('1-5: Drop new objects, Z: Activate/deactivate some bodies, X: Destroy an object');
+testbed.info(
+  "1-5: Drop new objects, Z: Activate/deactivate some bodies, X: Destroy an object"
+);
 
-testbed.step = function() {
+testbed.step = function () {
   AABBQueryListener.reset();
-  let aabb = new AABB();
+  const aabb = new AABB();
   AABBQueryListener.circle.computeAABB(aabb, AABBQueryListener.transform, 0);
 
   world.queryAABB(aabb, AABBQueryListener.callback);
 
-  testbed.drawCircle(AABBQueryListener.circle.m_p, AABBQueryListener.circle.m_radius, testbed.color(0.4, 0.7, 0.8));
+  testbed.drawCircle(
+    AABBQueryListener.circle.m_p,
+    AABBQueryListener.circle.m_radius,
+    testbed.color(0.4, 0.7, 0.8)
+  );
 };
 
 function drawFixture(fixture) {
-  let color = testbed.color(0.95, 0.95, 0.6);
-  let xf = fixture.getBody().getTransform();
+  const color = testbed.color(0.95, 0.95, 0.6);
+  const xf = fixture.getBody().getTransform();
 
   switch (fixture.getType()) {
-  case 'circle': {
-    let circle = fixture.getShape();
+    case "circle":
+      {
+        const circle = fixture.getShape();
 
-    let center = Transform.mul(xf, circle.getCenter());
-    let radius = circle.getRadius();
+        const center = Transform.mul(xf, circle.getCenter());
+        const radius = circle.getRadius();
 
-    testbed.drawCircle(center, radius, color);
-  }
-    break;
+        testbed.drawCircle(center, radius, color);
+      }
+      break;
 
-  case 'polygon': {
-    let poly = fixture.getShape();
-    let vertexCount = poly.m_count;
-    // assert(vertexCount <= b2_maxPolygonVertices);
-    let vertices = poly.m_vertices.map(v => Transform.mul(xf, v));
-    testbed.drawPolygon(vertices, color);
-  }
-    break;
+    case "polygon":
+      {
+        const poly = fixture.getShape();
+        const vertexCount = poly.m_count;
+        // assert(vertexCount <= b2_maxPolygonVertices);
+        const vertices = poly.m_vertices.map((v: planck.Vec2) =>
+          Transform.mul(xf, v)
+        );
+        testbed.drawPolygon(vertices, color);
+      }
+      break;
 
-  default:
-    break;
+    default:
+      break;
   }
 }
 
@@ -179,29 +201,36 @@ function drawFixture(fixture) {
 // that overlap an AABB. Of those, we use TestOverlap to determine which fixtures
 // overlap a circle. Up to 4 overlapped fixtures will be highlighted with a
 // yellow border.
-let AABBQueryListener = (function() {
-  let def = {};
+const AABBQueryListener = (function () {
+  const def: any = {};
 
   def.circle = new Circle(new Vec2(0.0, 1.1), 2.0);
   def.transform = new Transform();
   let count = 0;
 
-  let MAX_COUNT = 40;
+  const MAX_COUNT = 40;
 
-  def.reset = function() {
+  def.reset = function () {
     count = 0;
   };
   // Called for each fixture found in the query AABB.
   // return false to terminate the query.
-  def.callback = function(fixture) {
+  def.callback = function (fixture) {
     if (count === MAX_COUNT) {
       return false;
     }
 
-    let body = fixture.getBody();
-    let shape = fixture.getShape();
+    const body = fixture.getBody();
+    const shape = fixture.getShape();
 
-    let overlap = Distance.testOverlap(shape, 0, def.circle, 0, body.getTransform(), def.transform);
+    const overlap = Distance.testOverlap(
+      shape,
+      0,
+      def.circle,
+      0,
+      body.getTransform(),
+      def.transform
+    );
 
     if (overlap) {
       drawFixture(fixture);
