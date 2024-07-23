be7e922ec9d1c1cdd9ca257327eea58461b04429
Arsham <arsham.ramezani@gmail.com>
2024-07-23 00:55:37 -0700

    perf: code of mobile converted to typescript

--- example/Mobile.ts
+++ example/Mobile.ts
@@ -20,11 +20,11 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+import planck from "../src/main";
 
-const { World, Vec2, Box, RevoluteJoint, Testbed } = planck;
+const { Vec2, World, Box, RevoluteJoint, Testbed } = planck;
 
-
-let world = new World(new Vec2(0, -10));
+const world = new World(new Vec2(0, -1));
 
 const testbed = Testbed.mount();
 testbed.y = -15;
@@ -32,30 +32,37 @@ testbed.width = 20;
 testbed.height = 20;
 testbed.start(world);
 
-let DEPTH = 4;
-let DENSITY = 20.0;
+const DEPTH = 4;
+const DENSITY = 20.0;
 
-let ground = world.createBody(new Vec2(0.0, 20.0));
+const ground = world.createBody(new Vec2(0.0, 20.0));
 
-let a = 0.5;
-let h = new Vec2(0.0, a);
+const a = 0.5;
+const h = new Vec2(0.0, a);
 
-let root = addNode(ground, new Vec2(), 0, 3.0, a);
+const root = addNode(ground, new Vec2(), 0, 3.0, a);
 
-world.createJoint(new RevoluteJoint({
-  bodyA: ground,
-  bodyB: root,
-  localAnchorA : new Vec2(),
-  localAnchorB : h
-}));
+world.createJoint(
+  /// NEED TO FIX THIS
+  new RevoluteJoint(
+    {
+      bodyA: ground,
+      bodyB: root,
+      localAnchorA: new Vec2(0, 0),
+      localAnchorB: h,
+    },
+    ground,
+    root
+  )
+);
 
 function addNode(parent, localAnchor, depth, offset, a) {
+  const h = new Vec2(0.0, a);
 
-  let h = new Vec2(0.0, a);
-
-  let p = new Vec2(parent.getPosition()).add(localAnchor).sub(h);
-
-  let node = world.createDynamicBody(p);
+  const node = world.createBody({
+    type: "dynamic",
+    position: new Vec2(parent.getPosition()).add(localAnchor).sub(h),
+  });
 
   node.createFixture(new Box(0.25 * a, a), DENSITY);
 
@@ -63,26 +70,38 @@ function addNode(parent, localAnchor, depth, offset, a) {
     return node;
   }
 
-  node.createFixture(new Box(offset, 0.25 * a, new Vec2(0, -a), 0.0), DENSITY);
+  const left = new Vec2(offset, -a);
+  const right = new Vec2(-offset, -a);
+  const leftChild = addNode(node, left, depth + 1, 0.5 * offset, a);
+  const rightChild = addNode(node, right, depth + 1, 0.5 * offset, a);
 
-  let right = new Vec2(offset, -a);
-  let left = new Vec2(-offset, -a);
-  let rightChild = addNode(node, right, depth + 1, 0.5 * offset, a);
-  let leftChild = addNode(node, left, depth + 1, 0.5 * offset, a);
+  world.createJoint(
+    /// NEED TO FIX THIS
+    new RevoluteJoint(
+      {
+        bodyA: node,
+        bodyB: leftChild,
+        localAnchorA: left,
+        localAnchorB: h,
+      },
+      node,
+      leftChild
+    )
+  );
 
-  world.createJoint(new RevoluteJoint({
-    bodyA: node,
-    bodyB: rightChild,
-    localAnchorA: right,
-    localAnchorB: h,
-  }));
-
-  world.createJoint(new RevoluteJoint({
-    bodyA: node,
-    bodyB: leftChild,
-    localAnchorA: left,
-    localAnchorB: h,
-  }));
+  world.createJoint(
+    /// NEED TO FIX THIS
+    new RevoluteJoint(
+      {
+        bodyA: node,
+        bodyB: rightChild,
+        localAnchorA: right,
+        localAnchorB: h,
+      },
+      node,
+      rightChild
+    )
+  );
 
   return node;
 }
--- example/MobileBalanced.ts
+++ example/MobileBalanced.ts
@@ -21,9 +21,11 @@
  * SOFTWARE.
  */
 
-const { Vec2, World, Box, RevoluteJoint, Testbed } = planck;
+import planck from "../src/main";
 
-let world = new World(new Vec2(0, -1));
+const { World, Vec2, Box, RevoluteJoint, Testbed } = planck;
+
+const world = new World(new Vec2(0, -10));
 
 const testbed = Testbed.mount();
 testbed.y = -15;
@@ -31,31 +33,32 @@ testbed.width = 20;
 testbed.height = 20;
 testbed.start(world);
 
-let DEPTH = 4;
-let DENSITY = 20.0;
+const DEPTH = 4;
+const DENSITY = 20.0;
 
-let ground = world.createBody(new Vec2(0.0, 20.0));
+const ground = world.createBody(new Vec2(0.0, 20.0));
 
-let a = 0.5;
-let h = new Vec2(0.0, a);
+const a = 0.5;
+const h = new Vec2(0.0, a);
 
-let root = addNode(ground, new Vec2(), 0, 3.0, a);
+const root = addNode(ground, new Vec2(), 0, 3.0, a);
 
-world.createJoint(new RevoluteJoint({
-  bodyA: ground,
-  bodyB: root,
-  localAnchorA: new Vec2(0, 0),
-  localAnchorB: h,
-}, ground, root));
+world.createJoint(
+  /// NEED TO FIX THIS
+  new RevoluteJoint({
+    bodyA: ground,
+    bodyB: root,
+    localAnchorA: new Vec2(),
+    localAnchorB: h,
+  })
+);
 
 function addNode(parent, localAnchor, depth, offset, a) {
+  const h = new Vec2(0.0, a);
 
-  let h = new Vec2(0.0, a);
+  const p = new Vec2(parent.getPosition()).add(localAnchor).sub(h);
 
-  let node = world.createBody({
-    type : 'dynamic',
-    position : new Vec2(parent.getPosition()).add(localAnchor).sub(h)
-  });
+  const node = world.createDynamicBody(p);
 
   node.createFixture(new Box(0.25 * a, a), DENSITY);
 
@@ -63,24 +66,32 @@ function addNode(parent, localAnchor, depth, offset, a) {
     return node;
   }
 
-  let left = new Vec2(offset, -a);
-  let right = new Vec2(-offset, -a);
-  let leftChild = addNode(node, left, depth + 1, 0.5 * offset, a);
-  let rightChild = addNode(node, right, depth + 1, 0.5 * offset, a);
+  node.createFixture(new Box(offset, 0.25 * a, new Vec2(0, -a), 0.0), DENSITY);
 
-  world.createJoint(new RevoluteJoint({
-    bodyA: node,
-    bodyB: leftChild,
-    localAnchorA: left,
-    localAnchorB: h,
-  }, node, leftChild));
+  const right = new Vec2(offset, -a);
+  const left = new Vec2(-offset, -a);
+  const rightChild = addNode(node, right, depth + 1, 0.5 * offset, a);
+  const leftChild = addNode(node, left, depth + 1, 0.5 * offset, a);
 
-  world.createJoint(new RevoluteJoint({
-    bodyA: node,
-    bodyB: rightChild,
-    localAnchorA: right,
-    localAnchorB: h,
-  }, node, rightChild));
+  world.createJoint(
+    /// NEED TO FIX THIS
+    new RevoluteJoint({
+      bodyA: node,
+      bodyB: rightChild,
+      localAnchorA: right,
+      localAnchorB: h,
+    })
+  );
+
+  world.createJoint(
+    /// NEED TO FIX THIS
+    new RevoluteJoint({
+      bodyA: node,
+      bodyB: leftChild,
+      localAnchorA: left,
+      localAnchorB: h,
+    })
+  );
 
   return node;
 }
