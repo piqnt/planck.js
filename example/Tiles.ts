1cd7008d739bc9b2ba5ef3ab741e46532ea3433b
Arsham <arsham.ramezani@gmail.com>
2024-07-23 00:23:20 -0700

    perf: code of tiles converted to typescript

--- example/Tiles.ts
+++ example/Tiles.ts
@@ -23,26 +23,27 @@
 
 // This stress tests the dynamic tree broad-phase. This also shows that tile
 // based collision is _not_ smooth due to Box2D not knowing about adjacency.
+import planck from "../src/main";
 
 const { World, Vec2, Box, Testbed } = planck;
 
-let world = new World(new Vec2(0, -10));
+const world = new World(new Vec2(0, -10));
 
 const testbed = Testbed.mount();
 testbed.start(world);
 
-let COUNT = 20;
+const COUNT = 20;
 
 let fixtureCount = 0;
 
 {
-  let a = 0.5;
-  let ground = world.createBody(new Vec2(0, -a));
-
+  const a = 0.5;
+  const ground = world.createBody(new Vec2(0, -a));
+  /// NEED TO CHECK
   if (true) {
-    let N = 200;
-    let M = 10;
-    let position = new Vec2();
+    const N = 200;
+    const M = 10;
+    const position = new Vec2();
     position.y = 0.0;
     for (let j = 0; j < M; ++j) {
       position.x = -N * a;
@@ -53,11 +54,10 @@ let fixtureCount = 0;
       }
       position.y -= 2.0 * a;
     }
-
   } else {
-    let N = 200;
-    let M = 10;
-    let position = new Vec2();
+    const N = 200;
+    const M = 10;
+    const position = new Vec2();
     position.x = -N * a;
     for (let i = 0; i < N; ++i) {
       position.y = 0.0;
@@ -70,22 +70,21 @@ let fixtureCount = 0;
   }
 }
 {
-  let a = 0.5;
-  let shape = new Box(a, a);
+  const a = 0.5;
+  const shape = new Box(a, a);
 
-  let x = new Vec2(-7.0, 0.75);
-  let y = new Vec2();
-  let deltaX = new Vec2(0.5625, 1.25);
-  let deltaY = new Vec2(1.125, 0.0);
+  const x = new Vec2(-7.0, 0.75);
+  const y = new Vec2();
+  const deltaX = new Vec2(0.5625, 1.25);
+  const deltaY = new Vec2(1.125, 0.0);
 
   for (let i = 0; i < COUNT; ++i) {
     y.set(x);
 
     for (let j = i; j < COUNT; ++j) {
-
       // bd.allowSleep = !(i == 0 && j == 0)
 
-      let body = world.createDynamicBody(y);
+      const body = world.createDynamicBody(y);
       body.createFixture(shape, 5.0);
       ++fixtureCount;
       y.add(deltaY);
@@ -94,20 +93,20 @@ let fixtureCount = 0;
     x.add(deltaX);
   }
 }
-let createTime = Date.now();
+const createTime = Date.now();
 
-testbed.step = function() {
-  let height = world.getTreeHeight();
-  let leafCount = world.getProxyCount();
-  let minimumNodeCount = 2 * leafCount - 1;
-  let minimumHeight = Math.ceil(Math.log(minimumNodeCount) / Math.log(2.0));
+testbed.step = function () {
+  const height = world.getTreeHeight();
+  const leafCount = world.getProxyCount();
+  const minimumNodeCount = 2 * leafCount - 1;
+  const minimumHeight = Math.ceil(Math.log(minimumNodeCount) / Math.log(2.0));
 
-  testbed.status('dynamic tree height', height);
-  testbed.status('min', minimumHeight);
-  testbed.status('create time', createTime + 'ms');
-  testbed.status('fixture count', fixtureCount);
+  testbed.status("dynamic tree height", height);
+  testbed.status("min", minimumHeight);
+  testbed.status("create time", createTime + "ms");
+  testbed.status("fixture count", fixtureCount);
 
-  // let tree = world.m_broadPhase.m_tree;
+  // const tree = world.m_broadPhase.m_tree;
   // if (stepCount++ == 400) {
   // tree.rebuildBottomUp();
   // }
