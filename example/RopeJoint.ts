154974e0bcd9840fd2a371be11525f5649488fd9
Arsham <arsham.ramezani@gmail.com>
2024-07-23 01:00:40 -0700

    perf: code of ropejoint converted to typescript

--- example/RopeJoint.ts
+++ example/RopeJoint.ts
@@ -30,37 +30,40 @@
 // This test also shows how to use contact filtering. Filtering is configured
 // so that the payload does not collide with the chain.
 
+import planck from "../src/main";
+
 const { Vec2, World, Edge, Box, RevoluteJoint, RopeJoint, Testbed } = planck;
 
-let world = new World({x: 0, y: -10});
+/// NEED TO FIX THIS
+const world = new World({ x: 0, y: -10 });
 
 const testbed = Testbed.mount();
-testbed.info('X: Toggle the rope joint');
+testbed.info("X: Toggle the rope joint");
 testbed.start(world);
 
-let ground = world.createBody();
+const ground = world.createBody();
 
 ground.createFixture(new Edge(new Vec2(-40.0, 0.0), new Vec2(40.0, 0.0)), 0.0);
 
-let segmentDef = {
+const segmentDef = {
   density: 20.0,
   friction: 0.2,
   filterCategoryBits: 0x0001,
-  filterMaskBits: 0xFFFF & ~0x0002,
+  filterMaskBits: 0xffff & ~0x0002,
 };
 
-let segmentJointDef = {
+const segmentJointDef = {
   collideConnected: false,
 };
 
-let N = 10;
-let y = 15.0;
+const N = 10;
+const y = 15.0;
 
 let prevBody = ground;
 for (let i = 0; i < N; ++i) {
   let shape = new Box(0.5, 0.125);
-  let bd = {
-    type: 'dynamic',
+  const bd: any = {
+    type: "dynamic",
     position: new Vec2(0.5 + 1.0 * i, y),
   };
   if (i === N - 1) {
@@ -71,29 +74,31 @@ for (let i = 0; i < N; ++i) {
     bd.angularDamping = 0.4;
   }
 
-  let body = world.createBody(bd);
+  const body = world.createBody(bd);
 
   body.createFixture(shape, segmentDef);
 
-  let anchor = new Vec2(i, y);
+  const anchor = new Vec2(i, y);
   world.createJoint(new RevoluteJoint(segmentJointDef, prevBody, body, anchor));
 
   prevBody = body;
 }
 
-let ropeJointDef = {
+const ropeJointDef = {
   maxLength: N - 1.0 + 0.01,
   localAnchorA: new Vec2(0.0, y),
   localAnchorB: new Vec2(0, 0),
 };
+/// NEED TO FIX THIS
 let rope = world.createJoint(new RopeJoint(ropeJointDef, ground, prevBody));
 
-testbed.keydown = function(code, char) {
-  if (char === 'X') {
+testbed.keydown = function (code, char) {
+  if (char === "X") {
     if (rope) {
       world.destroyJoint(rope);
       rope = null;
     } else {
+      /// NEED TO FIX THIS
       rope = world.createJoint(new RopeJoint(ropeJointDef, ground, prevBody));
     }
   }
@@ -102,7 +107,7 @@ testbed.keydown = function(code, char) {
 };
 
 function updateStatus() {
-  testbed.status('Rope', !!rope);
+  testbed.status("Rope", !!rope);
 }
 
 updateStatus();
