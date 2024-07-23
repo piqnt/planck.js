2b63fa8d9e108f0523276e4a39e373b854134a58
Arsham <arsham.ramezani@gmail.com>
2024-07-23 00:28:35 -0700

    perf: code of varyingrestitutiin converted to typescript

--- example/VaryingRestitution.ts
+++ example/VaryingRestitution.ts
@@ -23,6 +23,7 @@
 
 // Note: even with a restitution of 1.0, there is some energy change
 // due to position correction.
+import planck from "../src/main";
 
 const { World, Vec2, Circle, Edge, Testbed } = planck;
 
@@ -34,7 +35,7 @@ testbed.start(world);
 const ground = world.createBody();
 ground.createFixture(new Edge(new Vec2(-40.0, 0.0), new Vec2(40.0, 0.0)));
 
-const restitution = [ 0.0, 0.1, 0.3, 0.5, 0.75, 0.9, 1.0 ];
+const restitution = [0.0, 0.1, 0.3, 0.5, 0.75, 0.9, 1.0];
 
 const circle = new Circle(1.0);
 
@@ -42,6 +43,6 @@ for (let i = 0; i < restitution.length; ++i) {
   const ball = world.createDynamicBody(new Vec2(-10.0 + 3.0 * i, 20.0));
   ball.createFixture(circle, {
     density: 1.0,
-    restitution: restitution[i]
+    restitution: restitution[i],
   });
 }
